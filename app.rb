2d2a311790f9c0ccad209a670cdb9288c542fae3
Alyssa Colistro <arcolistro@gmail.com>
2019-12-20 12:07:16 -0800

    Add inital html and erb scripts for integrating front end.

--- app.rb
+++ app.rb
@@ -1,3 +1,14 @@
 require('sinatra')
 require('sinatra/reloader')
+require('./lib/album')
+require('pry')
 also_reload('lib/**/*.rb')
+
+get('/test') do
+  erb(:word_definer)
+end
+
+get('/words') do
+  @words = Word.all
+  erb(:words)
+end
