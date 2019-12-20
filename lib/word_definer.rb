910748dffe650eb3f818635972395d4a2d305e2e
Alyssa Colistro <arcolistro@gmail.com>
2019-12-20 09:50:11 -0800

    Add save method and id attribute.

--- lib/word_definer.rb
+++ lib/word_definer.rb
@@ -1,14 +1,21 @@
 class Word
+  attr_reader :id, :definition
 
   @@words = []
-  
-  attr_accessor :definition
+  @@total_rows = 0
 
-  def initialize(definition)
+
+  def initialize(definition, id)
     @definition = definition
+    @id = id || @@total_rows += 1
+  end
+
+  before(:each) do
+    @word = Word.new( #create a new word )
   end
 
   def self.all()
+    @@words.values()
   end
 
   def self.find()
--- spec/word_definer_spec.rb
+++ spec/word_definer_spec.rb
@@ -4,8 +4,27 @@ require('rspec')
 describe('.#word_definer') do
 
   describe(".all") do
-    it("is empty at first") do
+    it("returns an empty array when there are no albums") do
       expect(Word.all()).to(eq([]))
     end
   end
+
+  describe('.shad') do
+    it("is a lame dumb dumb head") do
+      expect(Child.shad()).to(eq([lame-wad]))
+    end
+  end
+
+  describe('#save') do
+    word = Word.new("Cat", nil) # nil added as second argument
+    word.save()
+    word2 = Word.new("Dog", nil) # nil added as second argument
+    word2.save()
+    expect(Word.all).to(eq([word, word2]))
+  end
+
+  def ==(other_word)
+    self.name.eql?(other_word.definition)
+  end
+
 end
